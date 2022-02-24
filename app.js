9d6b05d199de2db7d88df0e9066e85a0de54819e
moosaa95 <moosaabdullahi45@gmail.com>
2022-02-24 11:03:27 +0100

    setting apollo servers on the app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+// imports from package files 
+const { ApolloServer } = require('apollo-server'); // this helps us to create a server
+// const gql = require('graphql-tag'); // this helps us to create a schema
+const mongoose = require('mongoose'); // this helps us to connect to mongodb
+
+
+//relative imports
+const { MONGODB } = require('./config'); // this is the database configuration
+const typeDefs = require('./graphql/typeDefs'); // this is the schema
+const resolvers = require('./graphql/resolvers'); // this is the resolvers
+
+
+//connect to mongodb
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true }) 
+    // promise to listen to the port
+    .then(() => {
+        console.log('Connected to mongodb')
+        return server.listen({ port: 4000 })
+    })
+    .then((res) => {
+        console.log(`Server is running on port ${res.port}`)
+    })
+
+
+
+// server to connect typeDefs and resolvers
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+
+// server.listen().then(({ url }) => {
+//     console.log(`Server ready at ${url}`);
+// }   
+// );
+
